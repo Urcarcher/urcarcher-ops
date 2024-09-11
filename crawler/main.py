e8b3c7cfbe1d2a3f2bafe8f6abed22ff0c51ccf0
1nthebleakmidwinter <1nth2bleakmidwinter@gmail.com>
2024-09-11 14:29:45 +0900

    fe nginx jenkins config 수정

--- crawler/main.py
+++ crawler/main.py
@@ -9,20 +9,20 @@ from .realtime_config import EXCHANGE_TYPES
 from .realtime_config import PROCESS_BOUNDARY
 
 if __name__ == "__main__" :
-    crawlers = [RealTimeExRateCrawler.remote(
-                    CRAWLER_CONFIG['backend_blue_1'], EXCHANGE_TYPES, 
-                    0, 58
-                ),
-                RealTimeExRateCrawler.remote(
-                    CRAWLER_CONFIG['backend_blue_2'], EXCHANGE_TYPES, 
-                    0, 58
-                )]
     # crawlers = [RealTimeExRateCrawler.remote(
-    #                 CRAWLER_CONFIG['backend_green_1'], EXCHANGE_TYPES, 
+    #                 CRAWLER_CONFIG['backend_blue_1'], EXCHANGE_TYPES, 
     #                 0, 58
     #             ),
     #             RealTimeExRateCrawler.remote(
-    #                 CRAWLER_CONFIG['backend_green_2'], EXCHANGE_TYPES, 
+    #                 CRAWLER_CONFIG['backend_blue_2'], EXCHANGE_TYPES, 
     #                 0, 58
     #             )]
+    crawlers = [RealTimeExRateCrawler.remote(
+                    CRAWLER_CONFIG['backend_green_1'], EXCHANGE_TYPES, 
+                    0, 58
+                ),
+                RealTimeExRateCrawler.remote(
+                    CRAWLER_CONFIG['backend_green_2'], EXCHANGE_TYPES, 
+                    0, 58
+                )]
     _ = ray.get([crawler.run.remote() for crawler in crawlers])
